fac874ee2e272499a50154f6e237f17f95c16211
Coder-12 <31615917+Coder-12@users.noreply.github.com>
2020-11-20 18:09:52 +0530

    Update Extractive_BERT.py

--- Extractive_BERT.py
+++ Extractive_BERT.py
@@ -1,8 +1,13 @@
 # %%
 from summarizer import Summarizer
 from pipelines import pipeline
+from transformers import BertTokenizer, BertForQuestionAnswering
+import torch
 import sys
 
+# %%
+tokenizer =  BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertForQuestionAnswering.from_pretrained('bert-base-uncased', return_dict=True)
 # %%
 def getdata(filename):
     data=""
@@ -27,6 +32,7 @@ result = model(body, min_length=60)
 nlp = pipeline("question-generation")
 
 # %%
+outfile=open(sys.argv[2],'w')
 sentences = result.split("\n")
 
 for sent in sentences:
@@ -40,7 +46,19 @@ for sent in sentences:
     for x in res:
         q, a = x["question"], x["answer"]
 
-        print(q, "\t", a)
+        # Bert Model to predict answer given question
+        input_ids = tokenizer.encode(q, a)
+        sep_index = input_ids.index(tokenizer.sep_token_id)
+        num_seg_a = sep_index + 1
+        num_seg_b = len(input_ids) - num_seg_a
+        segment_ids = [0]*num_seg_a + [1]*num_seg_b
+        start_scores, end_scores = model(torch.tensor([input_ids]),token_type_ids=torch.tensor([segment_ids]))
+        answer_start = torch.argmax(start_scores)
+        answer_end = torch.argmax(end_scores)
+        predicted_answer = ' '.join(tokens[answer_start:answer_end+1])
+        outfile.write(q+'\n'+predicted_answer)
+       
+
 
 # %%
 
